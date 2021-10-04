e86d69df6fb6d72c3cea225b0380c4b945518a46
attackoncs <2710651793@qq.com>
2021-10-04 21:19:05 +0800

    update missing semester Shell Tools and Scripting to LinuxBasic

--- shell.sh
+++ shell.sh
@@ -2,6 +2,7 @@
 概论
 学习技巧
 不要死记硬背，遇到含糊不清的地方，可以在AC Terminal里实际运行一遍。
+编写 bash 脚本有时候会很别扭和反直觉。例如 shellcheck这样的工具可以帮助你定位sh/bash脚本中的错误。
 脚本示例
 新建一个test.sh文件，内容如下：
 #! /bin/bash
@@ -16,6 +17,22 @@ Hello World!  # 脚本输出
 acs@9e0ebfcd82d7:~$ ~/test.sh  # 家目录路径下执行
 Hello World!  # 脚本输出
 用解释器执行
+
+此外，注意：脚本并不一定只有用bash写才能调用，如python脚本，将输入参数倒序输出：
+内核知道去用python解释器而不是shell命令来运行这段脚本，是因为脚本的开头第一行的shebang。
+在 shebang 行中使用 env 命令是一种好的实践，它会利用环境变量中的程序来解析该脚本，这样就提高来您的脚本的可移植性。
+env 会利用我们第一节讲座中介绍过的PATH 环境变量来进行定位。 例如，使用了env的shebang看上去时这样的#!/usr/bin/env python。
+#!/usr/local/bin/python
+import sys
+for arg in reversed(sys.argv[1:]):
+    print(arg)
+
+shell函数和脚本有如下一些不同点：
+函数只能用与shell使用相同的语言，脚本可以使用任意语言。因此在脚本中包含 shebang 是很重要的。
+函数仅在定义时被加载，脚本会在每次被执行时加载。这让函数的加载比脚本略快一些，但每次修改函数定义，都要重新加载一次。
+函数会在当前的shell环境中执行，脚本会在单独的进程中执行。因此，函数可以对环境变量进行更改，比如改变当前工作目录，脚本则不行。
+脚本需要使用 export 将环境变量导出，并将值传递给环境变量。
+与其他程序语言一样，函数可以提高代码模块性、代码复用性并创建清晰性的结构。shell脚本中往往也会包含它们自己的函数定义。
 EOF
 
 :<<EOF
@@ -58,6 +75,13 @@ name=yxc
 echo 'hello,$name \"hh\"'
 echo "hello,$name \"hh\""
 
+''不会替换变量值但双引号会
+foo=bar
+echo "$foo"
+# prints bar
+echo '$foo'
+# prints $foo
+
 #获取字符串长度
 echo ${#name}
 #提取子串
@@ -72,17 +96,65 @@ echo "文件名："$o
 echo "第一个参数"$1
 echo "第二个参数"$2
 echo "第三个参数"$3
-
+命令通常使用 STDOUT来返回输出值，使用STDERR 来返回错误及错误码
 其它参数相关变量
 参数	说明
 $#	代表文件传入的参数个数，如上例中值为4
 $*	由所有参数构成的用空格隔开的字符串，如上例中值为"$1 $2 $3 $4"
 $@	每个参数分别用双引号括起来的字符串，如上例中值为"$1" "$2" "$3" "$4"
 $$	脚本当前运行的进程ID
-$?	上一条命令的退出状态（注意不是stdout，而是exit code）。0表示正常退出，其他值表示错误
+$?	上一条命令的返回值（注意不是stdout，而是exit code）。0表示正常退出，其他值表示错误
 $(command)	返回command这条命令的stdout（可嵌套）
 `command`	返回command这条命令的stdout（不可嵌套）
+$$ 当前脚本进程的PID
+!! 完整的上一条命令，常见应用：当你因为权限不足执行命令失败时，可以使用 sudo !!再尝试一次。
+$_ - 上一条命令的最后一个参数。如果你正在使用的是交互式shell，你可以通过按下 Esc 之后键入 . 来获取这个值。
+
+命令替换和进程替换
+另一个常见的模式是以变量的形式获取一个命令的输出，当通过$(command)执行command时，输出结果会替换掉$(command)
+如for file in $(ls)先调用ls，再遍历返回值
+还有另外冷门特性是进程替换，<(command)会执行cmd并将结果输出到临时文件，并将<(cmd)替换成临时文件名，希望返回值
+通过文件而不是STDIN传递时很有用。如diff <(ls foo) <(ls bar)会显示foo和bar文件夹的区别
+
+例子：脚本遍历提供参数，用grep搜索字符串foobar，若没找到将其作为注释追加到文件中
+#!/bin/bash
+echo "Starting program at $(date)" # date会被替换成日期和时间
+echo "Running program $0 with $# arguments with pid $$"
+for file in "$@"; do
+    grep foobar "$file" > /dev/null 2> /dev/null
+    # 如果模式没有找到，则grep退出状态为 1
+    # 我们将标准输出流和标准错误流重定向到Null，因为我们并不关心这些信息
+    #在bash中进行比较时，尽量使用双方括号 [[ ]] 而不是单方括号 [ ]
+    if [[ $? -ne 0 ]]; then
+        echo "File $file does not have any foobar, adding one"
+        echo "# foobar" >> "$file"
+    fi
+done
+
+通配符
+?和*分别匹配一个或人一个字符
+{}当有一些列指令，其中包含公共子串时，可用花括号自动展开这些命令，这在批量移动或转换文件时很方便
+convert image.{png,jpg}
+# 会展开为
+convert image.png image.jpg
+
+cp /path/to/project/{foo,bar,baz}.sh /newpath
+# 会展开为
+cp /path/to/project/foo.sh /path/to/project/bar.sh /path/to/project/baz.sh /newpath
+
+# 也可以结合通配使用
+mv *{.py,.sh} folder
+# 会移动所有 *.py 和 *.sh 文件
+
+mkdir foo bar
+
+# 下面命令会创建foo/a, foo/b, ... foo/h, bar/a, bar/b, ... bar/h这些文件
+touch {foo,bar}/{a..h}
+touch foo/x bar/y
+# 比较文件夹 foo 和 bar 中包含文件的不同
+diff <(ls foo) <(ls bar)
 EOF
+
 
 #数组
 :<<EOF
@@ -488,4 +560,70 @@ name=yxc  # 定义变量name
 #! /bin/bash
 source test1.sh # 或 . test1.sh
 echo My name is: $name  # 可以使用test1.sh中的变量
+EOF
+
+:<<EOF
+查找文件
+程序员们面对的最常见的重复任务就是查找文件或目录。所有的类UNIX系统都包含一个名为 find的工具，它是shell上用于查找文件的绝佳工具。
+find命令会递归地搜索符合条件的文件，例如：
+
+# 查找所有名称为src的文件夹
+find . -name src -type d
+# 查找所有文件夹路径中包含test的python文件
+find . -path '*/test/*.py' -type f
+# 查找前一天修改的所有文件
+find . -mtime -1
+# 查找所有大小在500k至10M的tar.gz文件
+find . -size +500k -size -10M -name '*.tar.gz'
+
+除了列出所寻找的文件之外，find还能对所有查找到的文件进行操作。这能极大地简化一些单调的任务。
+# 删除全部扩展名为.tmp 的文件
+find . -name '*.tmp' -exec rm {} \;
+# 查找全部的 PNG 文件并将其转换为 JPG
+find . -name '*.png' -exec convert {} {}.jpg \;
+
+尽管 find 用途广泛，它的语法却比较难以记忆。例如，为了查找满足模式 PATTERN 的文件，您需要执行 find -name '*PATTERN*' (如果您希望模式匹配时是不区分大小写，可以使用-iname选项）
+当然可用alias设置别名简化操作，但shell的哲学之一便是寻找（更好用的）替代方案。 shell最好的特性就是您只是在调用程序，因此只要找到合适的替代程序即可（甚至自己编写）。
+例如， fd 就是一个更简单、更快速、更友好的程序，它可以用来作为find的替代品。它有很多不错的默认设置，例如输出着色、默认支持正则匹配、支持unicode并且我认为它的语法更符合直觉。
+以模式PATTERN 搜索的语法是 fd PATTERN。大多数人都认为 find 和 fd 已经很好用了，但是有的人可能想知道，我们是不是可以有更高效的方法，例如不要每次都搜索文件而是
+通过编译索引或建立数据库的方式来实现更加快速地搜索。这就要靠 locate 了。 locate 使用一个由 updatedb负责更新的数据库，在大多数系统中 updatedb 都会通过 cron每日更新。
+这便需要我们在速度和时效性之间作出权衡。而且，find 和类似的工具可以通过别的属性比如文件大小、修改时间或是权限来查找文件，locate则只能通过文件名。
+重要的是你要知道有些问题使用合适的工具就会迎刃而解，而具体选择哪个工具则不是那么重要。
+EOF
+
+:<<EOF
+查找代码
+一个最常见的场景是您希望查找具有某种模式的全部文件，并找它们的位置。
+为了实现这一点，很多类UNIX的系统都提供了grep命令，它是用于对输入文本进行匹配的通用工具。
+-C ：获取查找结果的上下文（Context）；-v 将对结果进行反选（Invert），也就是输出不匹配的结果
+-R 会递归地进入子目录并搜索所有的文本文件,我们有很多办法可以对 grep -R 进行改进，例如使其忽略.git 文件夹，使用多CPU等等。
+因此也出现了很多它的替代品，包括 ack, ag 和 rg。它们都特别好用，但是功能也都差不多，我比较常用的是 ripgrep (rg) ，因为它速度快，而且用法非常符合直觉。例子如下：
+# 查找所有使用了 requests 库的文件
+rg -t py 'import requests'
+# 查找所有没有写 shebang 的文件（包含隐藏文件）
+rg -u --files-without-match "^#!"
+# 查找所有的foo字符串，并打印其之后的5行
+rg foo -A 5
+# 打印匹配的统计信息（匹配的行和文件的数量）
+rg --stats PATTERN
+EOF
+
+:<<EOF
+查找shell命令
+随着用shell越久，可能要查找之前输入过的命令
+1向上方向键
+2history | grep command
+可以修改 shell history 的行为，例如，如果在命令的开头加上一个空格，它就不会被加进shell记录中。当你输入包含密码或是其他敏感信息的命令时会用到这一特性。 
+为此你需要在.bashrc中添加HISTCONTROL=ignorespace
+或者向.zshrc 添加 setopt HIST_IGNORE_SPACE。 如果你不小心忘了在前面加空格，可以通过编辑。bash_history或 .zhistory 来手动地从历史记录中移除那一项。
+3Ctrl+R 输入子串进行匹配，查找历史命令行，反复按就会在所有搜素结果中循环，在 zsh中，使用方向键上或下也可以完成这项工作。
+4Ctrl+R 可以配合 fzf 使用。fzf 是一个通用对模糊查找工具，它可以和很多命令一起使用。
+5基于历史的自动补全，zsh可以使用极大提高用户体验
+EOF
+
+:<<EOF
+文件夹导航
+如何才能高效地在目录 间随意切换呢？有很多简便的方法可以做到，比如设置alias，使用 ln -s创建符号连接等。而开发者们已经想到了很多更为精妙的解决方案。
+可以使用fasd和autojump这两个工具来查找最常用或最近使用的文件和目录，Fasd 基于 frecency对文件和文件排序，也就是说它会同时针对频率（frequency ）和时效（ recency）进行排序
+例如， 如果您经常访问/home/user/files/cool_project 目录，那么可以直接使用 z cool 跳转到该目录。对于 autojump，则使用j cool代替即可。
 EOF
